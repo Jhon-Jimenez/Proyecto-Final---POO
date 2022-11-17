10ae2d09f1e6afce2d7eb2af055b37131afb2555
Jhon-Jimenez <zuletajj@uninorte.edu.co>
2022-11-17 01:13:37 -0500

    Update

--- Classes.py
+++ Classes.py
@@ -6,19 +6,29 @@ class Usuario:
         self.cc = cc
         self.dinero = dinero
 
-    def registrar_cliente(self) -> None:
+    def registrar_usuario(self) -> None:
         print("Hola usuario")
-        print("Usted seleccionó registrar un nuevo cliente")
+        print("Usted seleccionó registrar un nuevo usuario")
         print("Por favor ingrese los siguientes datos:")
         nombre = str(input("\nIngrese su nombre\n"))
         apellido = str(input("\nIngrese su apellido\n"))
-        cc = int(input("\nIngrese su cedula\n"))
+        cc = int(input("\nIngrese su cedula, tenga en cuenta que esta será su número de cuenta\n"))
         dinero = float(input("\nIngrese la cantidad de dinero al abrir su cuenta\n"))
         print("\nNuevo cliente registrado\n")
 
-        lista_clientes = []
+        lista_usuarios = []
         nuevo = Usuario(nombre,apellido,cc,dinero)
-        lista_clientes.append(nuevo)
+        lista_usuarios.append(nuevo)
+
+    def ver_lista_usuarios(self) -> None:
+        print("Hola usuario")
+        print("Usted seleccionó ver la lista de usuarios registrados\n")
+        
+    def buscar_usuario(self) -> None:
+        print("Hola usuario")
+        print("Usted seleccionó buscar usuario")
+        cc = int(input("Ingrese el número de cuenta o cedula del usuario a buscar"))
+        
 
 class Banco:
     def __init__(self, ingreso: float, retiro: float) -> None:
